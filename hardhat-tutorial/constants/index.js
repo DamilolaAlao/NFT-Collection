10bb7abc2c4245dd861038a508ea8ffc1eaef98a
Damilola Alao <oluwadamilola.alao@gmail.com>
2022-02-25 21:53:54 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x27CC22fe19b4799CBc3b288031e47Ca3f20Ce63E";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
